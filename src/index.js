7a458676609860893f98cce11905414936965e8a
Gabi <ggabrielagoncalvess@gmail.com>
2025-06-11 16:14:13 -0300

    adicionei desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-let nome = "felipe"
-console.log(nome)
+// Dados de entrada
+let nome = "Mulher Maravilha";
+let xp = 5894;
+let nivel = "";
+
+// Estrutura de decisão
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp <= 5000) {
+  nivel = "Prata";
+} else if (xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp <= 8000) {
+  nivel = "Platina";
+} else if (xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// Laço de repetição 
+for (let i = 0; i < 1; i++) {
+ console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+}
